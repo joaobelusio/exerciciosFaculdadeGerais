b9ea9930a37f8d51159fbe4321dbf12df7f1f25c
João Belusio <97858504+joaobelusio@users.noreply.github.com>
2024-05-08 23:59:31 -0300

    Create main.java

---
+++ ProgramacaoSolucoesComputacionais-1Semestre/Leonardo/Lista07/exercicio01/main.java
@@ -0,0 +1,8 @@
+package ProgramacaoSolucoesComputacionais-1Semestre.Leonardo.Lista07.exercicio01;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("teste");
+    }
+    
+}
